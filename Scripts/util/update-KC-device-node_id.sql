e9624e51838385e79120757950f12168caa65454
"Lawrence Lai" <javalai@gmail.com>
2025-04-07 23:01:39 +0800

    更新昆山廠裝置的Node ID。

---
+++ Scripts/util/update-KC-device-node_id.sql
@@ -0,0 +1,157 @@
+-- 昆山
+
+-- 用电维度
+UPDATE hd_device SET node_id='10041000000' WHERE device_id='1-8L';
+UPDATE hd_device SET node_id='10041000002' WHERE device_id='2-1L';
+UPDATE hd_device SET node_id='10041000003' WHERE device_id='2-4L';
+UPDATE hd_device SET node_id='10041000004' WHERE device_id='2-3L';
+UPDATE hd_device SET node_id='10041000006' WHERE device_id='1-3L';
+UPDATE hd_device SET node_id='10041000007' WHERE device_id='1-5L';
+UPDATE hd_device SET node_id='10041000008' WHERE device_id='1-6L';
+UPDATE hd_device SET node_id='10041000010' WHERE device_id='2-5L';
+UPDATE hd_device SET node_id='10041000011' WHERE device_id='2-2L';
+UPDATE hd_device SET node_id='10041000013' WHERE device_id='3-1L';
+UPDATE hd_device SET node_id='10041000058' WHERE device_id='1-10L';
+UPDATE hd_device SET node_id='10041000060' WHERE device_id='4-7L';
+UPDATE hd_device SET node_id='10041000061' WHERE device_id='4-8L';
+UPDATE hd_device SET node_id='10041000063' WHERE device_id='23-1L';
+UPDATE hd_device SET node_id='10041000054' WHERE device_id='9-11L';
+UPDATE hd_device SET node_id='10041000055' WHERE device_id='9-12L';
+UPDATE hd_device SET node_id='10041000056' WHERE device_id='9-13L';
+UPDATE hd_device SET node_id='10041000057' WHERE device_id='9-14L';
+UPDATE hd_device SET node_id='10041000065' WHERE device_id='11-9L';
+UPDATE hd_device SET node_id='10041000050' WHERE device_id='22-1L';
+UPDATE hd_device SET node_id='10041000051' WHERE device_id='22-4L';
+UPDATE hd_device SET node_id='10041000052' WHERE device_id='22-2L';
+UPDATE hd_device SET node_id='10041000053' WHERE device_id='22-3L';
+UPDATE hd_device SET node_id='10041000049' WHERE device_id='18-4L';
+
+-- 用水维度
+UPDATE hd_device SET node_id='10051000000' WHERE device_id='18-3L';
+UPDATE hd_device SET node_id='10051000003' WHERE device_id='6-8L,6-9L';
+UPDATE hd_device SET node_id='10051000004' WHERE device_id='6-7L';
+UPDATE hd_device SET node_id='10051000005' WHERE device_id='6-10L';
+UPDATE hd_device SET node_id='10051000006' WHERE device_id='6-12L';
+UPDATE hd_device SET node_id='10051000007' WHERE device_id='6-11L';
+UPDATE hd_device SET node_id='10051000008' WHERE device_id='6-3L';
+UPDATE hd_device SET node_id='10051000009' WHERE device_id='6-4L';
+UPDATE hd_device SET node_id='10051000010' WHERE device_id='6-1L';
+UPDATE hd_device SET node_id='10051000011' WHERE device_id='6-2L';
+UPDATE hd_device SET node_id='10051000012' WHERE device_id='6-5L';
+UPDATE hd_device SET node_id='10051000030' WHERE device_id='19-1L';
+UPDATE hd_device SET node_id='10051000031' WHERE device_id='19-2L';
+UPDATE hd_device SET node_id='10051000013' WHERE device_id='6-13L';
+UPDATE hd_device SET node_id='10051000018' WHERE device_id='22-5L';
+UPDATE hd_device SET node_id='10051000019' WHERE device_id='18-5L';
+UPDATE hd_device SET node_id='10051000015' WHERE device_id='6-6L';
+UPDATE hd_device SET node_id='10051000016' WHERE device_id='14-1L';
+UPDATE hd_device SET node_id='10051000020' WHERE device_id='12-2L';
+UPDATE hd_device SET node_id='10051000021' WHERE device_id='12-3L';
+UPDATE hd_device SET node_id='10051000022' WHERE device_id='12-4L';
+UPDATE hd_device SET node_id='10051000023' WHERE device_id='12-5L';
+UPDATE hd_device SET node_id='10051000024' WHERE device_id='12-6L';
+UPDATE hd_device SET node_id='10051000025' WHERE device_id='12-7L';
+UPDATE hd_device SET node_id='10051000026' WHERE device_id='12-8L';
+UPDATE hd_device SET node_id='10051000027' WHERE device_id='12-10L';
+UPDATE hd_device SET node_id='10051000028' WHERE device_id='12-11L';
+
+-- 空气维度
+UPDATE hd_device SET node_id='10071000001' WHERE device_id='8-1L';
+UPDATE hd_device SET node_id='10071000003' WHERE device_id='8-3L';
+UPDATE hd_device SET node_id='10071000004' WHERE device_id='8-4L';
+UPDATE hd_device SET node_id='10071000005' WHERE device_id='8-5L';
+UPDATE hd_device SET node_id='10071000006' WHERE device_id='8-6L';
+UPDATE hd_device SET node_id='10071000007' WHERE device_id='8-7L';
+UPDATE hd_device SET node_id='10071000008' WHERE device_id='8-8L';
+UPDATE hd_device SET node_id='10071000009' WHERE device_id='8-9L';
+UPDATE hd_device SET node_id='10071000010' WHERE device_id='8-15L';
+UPDATE hd_device SET node_id='10071000012' WHERE device_id='8-11L';
+UPDATE hd_device SET node_id='10071000013' WHERE device_id='8-12L';
+UPDATE hd_device SET node_id='10071000014' WHERE device_id='8-13L';
+UPDATE hd_device SET node_id='10071000015' WHERE device_id='8-14L';
+UPDATE hd_device SET node_id='10071000016' WHERE device_id='6-14L';
+UPDATE hd_device SET node_id='10071000017' WHERE device_id='6-15L';
+UPDATE hd_device SET node_id='10071000018' WHERE device_id='6-16L';
+UPDATE hd_device SET node_id='10071000019' WHERE device_id='6-20L';
+UPDATE hd_device SET node_id='10071000020' WHERE device_id='6-17L';
+UPDATE hd_device SET node_id='10071000021' WHERE device_id='6-19L';
+UPDATE hd_device SET node_id='10071000022' WHERE device_id='6-18L';
+UPDATE hd_device SET node_id='10071000002' WHERE device_id='8-2L';
+UPDATE hd_device SET node_id='10071000011' WHERE device_id='8-10L';
+
+-- 氮气维度
+UPDATE hd_device SET node_id='10141000002' WHERE device_id='24-2L';
+UPDATE hd_device SET node_id='10141000003' WHERE device_id='24-3L';
+
+-- 蒸汽维度
+UPDATE hd_device SET node_id='10061000000' WHERE device_id='18-1L';
+UPDATE hd_device SET node_id='10061000001' WHERE device_id='13-11L';
+UPDATE hd_device SET node_id='10061000002' WHERE device_id='13-12L';
+UPDATE hd_device SET node_id='10061000003' WHERE device_id='13-13L';
+UPDATE hd_device SET node_id='10061000004' WHERE device_id='13-14L';
+UPDATE hd_device SET node_id='10061000005' WHERE device_id='13-15L';
+UPDATE hd_device SET node_id='10061000006' WHERE device_id='13-16L';
+UPDATE hd_device SET node_id='10061000007' WHERE device_id='13-17L';
+UPDATE hd_device SET node_id='10061000008' WHERE device_id='13-18L';
+UPDATE hd_device SET node_id='10061000009' WHERE device_id='13-19L';
+UPDATE hd_device SET node_id='10061000010' WHERE device_id='10-7L';
+UPDATE hd_device SET node_id='10061000011' WHERE device_id='14-6L';
+UPDATE hd_device SET node_id='10061000013' WHERE device_id='13-20L';
+UPDATE hd_device SET node_id='10061000014' WHERE device_id='2-7L';
+UPDATE hd_device SET node_id='10061000015' WHERE device_id='2-8L';
+UPDATE hd_device SET node_id='10061000012' WHERE device_id='18-2L';
+
+-- 重点设备
+UPDATE hd_device SET node_id='10081000055' WHERE device_id='15-1L';
+UPDATE hd_device SET node_id='10081000056' WHERE device_id='15-2L';
+UPDATE hd_device SET node_id='10081000057' WHERE device_id='15-3L';
+UPDATE hd_device SET node_id='10081000058' WHERE device_id='15-4L';
+UPDATE hd_device SET node_id='10081000059' WHERE device_id='15-5L';
+UPDATE hd_device SET node_id='10081000060' WHERE device_id='15-6L';
+UPDATE hd_device SET node_id='10081000061' WHERE device_id='15-7L';
+UPDATE hd_device SET node_id='10081000062' WHERE device_id='15-8L';
+UPDATE hd_device SET node_id='10081000063' WHERE device_id='15-9L';
+UPDATE hd_device SET node_id='10081000064' WHERE device_id='16-10L';
+UPDATE hd_device SET node_id='10081000065' WHERE device_id='16-11L';
+UPDATE hd_device SET node_id='10081000066' WHERE device_id='15-12L';
+UPDATE hd_device SET node_id='10081000067' WHERE device_id='16-13L';
+UPDATE hd_device SET node_id='10081000068' WHERE device_id='16-14L';
+UPDATE hd_device SET node_id='10081000069' WHERE device_id='15-15L';
+UPDATE hd_device SET node_id='10081000070' WHERE device_id='16-16L';
+UPDATE hd_device SET node_id='10081000071' WHERE device_id='16-17L';
+UPDATE hd_device SET node_id='10081000072' WHERE device_id='16-18L';
+UPDATE hd_device SET node_id='10081000073' WHERE device_id='17-19L';
+UPDATE hd_device SET node_id='10081000074' WHERE device_id='17-20L';
+UPDATE hd_device SET node_id='10081000075' WHERE device_id='17-21L';
+UPDATE hd_device SET node_id='10081000076' WHERE device_id='14-22L';
+
+-- 环保设备
+UPDATE hd_device SET node_id='10121000001' WHERE device_id='21-1L';
+UPDATE hd_device SET node_id='10121000002' WHERE device_id='21-2L,27-1L';
+UPDATE hd_device SET node_id='10121000003' WHERE device_id='21-3L';
+UPDATE hd_device SET node_id='10121000004' WHERE device_id='20-1L';
+UPDATE hd_device SET node_id='10121000005' WHERE device_id='20-2L';
+UPDATE hd_device SET node_id='10121000006' WHERE device_id='20-3L';
+UPDATE hd_device SET node_id='10121000007' WHERE device_id='1-9L';
+UPDATE hd_device SET node_id='10121000008' WHERE device_id='3-2L';
+UPDATE hd_device SET node_id='10121000009' WHERE device_id='10-6L';
+UPDATE hd_device SET node_id='10121000010' WHERE device_id='3-3L';
+UPDATE hd_device SET node_id='10121000011' WHERE device_id='25-1L';
+UPDATE hd_device SET node_id='10121000012' WHERE device_id='9-16L';
+UPDATE hd_device SET node_id='10121000013' WHERE device_id='9-18L';
+UPDATE hd_device SET node_id='10121000014' WHERE device_id='9-19L';
+UPDATE hd_device SET node_id='10121000015' WHERE device_id='9-20L';
+UPDATE hd_device SET node_id='10121000016' WHERE device_id='9-21L';
+UPDATE hd_device SET node_id='10121000017' WHERE device_id='9-17L';
+UPDATE hd_device SET node_id='10121000018' WHERE device_id='16-19L';
+UPDATE hd_device SET node_id='10121000019' WHERE device_id='16-20L';
+UPDATE hd_device SET node_id='10121000020' WHERE device_id='16-21L';
+UPDATE hd_device SET node_id='10121000021' WHERE device_id='16-22L';
+UPDATE hd_device SET node_id='10121000022' WHERE device_id='16-23L';
+UPDATE hd_device SET node_id='10121000023' WHERE device_id='16-24L';
+UPDATE hd_device SET node_id='10121000024' WHERE device_id='17-1L';
+UPDATE hd_device SET node_id='10121000025' WHERE device_id='17-2L';
+UPDATE hd_device SET node_id='10121000026' WHERE device_id='17-3L';
+UPDATE hd_device SET node_id='10121000027' WHERE device_id='17-4L';
+UPDATE hd_device SET node_id='10121000028' WHERE device_id='26-1L';
+UPDATE hd_device SET node_id='10121000029' WHERE device_id='26-2L';
