b9dfa763df4a7628db1292c41d2b0a1754061456
"Lawrence Lai" <javalai@gmail.com>
2025-04-11 00:36:27 +0800

    提交非電能源的耗用量時統計程式。並取消對 entity_id 的使用。

---
+++ Scripts/sp_update_other_energy_stat_hour_sub.sql
@@ -0,0 +1,135 @@
+-- DROP PROCEDURE public.sp_update_other_energy_stat_hour_sub(text);
+
+CREATE OR REPLACE PROCEDURE public.sp_update_other_energy_stat_hour_sub(IN p_device_id text)
+ LANGUAGE plpgsql
+AS $procedure$
+
+  DECLARE 
+
+    v_rec RECORD;
+    v_nrec RECORD;
+    v_energy_key_id INT;
+    v_consumed_energy_key_id INT;
+    is_last BOOLEAN := FALSE;
+    v_start_time TIMESTAMP;
+    v_duration FLOAT;
+
+    v_ins_rows NUMERIC := 0;
+
+    v_cursor CURSOR FOR 
+      SELECT 
+        DATE_TRUNC('hour', hdsm.stat_time) AS truncated_stat_time,
+        hdsm.device_id, 
+        2 AS stat_type, -- 時統計
+        hdsm.device_type,
+        hdsm.key_id,
+        SUM(hdsm.dbl_stats) AS dbl_stats,
+        COALESCE(AVG(hdsm.dbl_stats),0) AS dbl_avg,
+        COALESCE(MIN(hdsm.dbl_stats),0) AS dbl_min,
+        COALESCE(MAX(hdsm.dbl_stats),0) AS dbl_max,
+        -- SUM(hdsm.dbl_stats) * COALESCE(hcp.float_value, 0)
+        CASE
+          WHEN hkc.emission_coeff IS NOT NULL THEN
+            SUM(hdsm.dbl_stats) * COALESCE(hcp.float_value, 0)
+          ELSE 0
+        END AS kgco2e
+      FROM hd_device hd
+      JOIN hd_device_statistics_minutely hdsm ON hdsm.device_id = hd.device_id
+      LEFT JOIN hd_key_config hkc ON hkc.device_type = hd.device_type -- 能源 Key 設定表
+      JOIN key_dictionary kd ON kd.key_id = hdsm.key_id
+      JOIN hd_config_param hcp ON hcp.factory_id = hd.factory_id -- 碳排係數表
+      LEFT JOIN hd_device_stat_hour_latest latest ON latest.device_id=hdsm.device_id AND latest.device_type = hdsm.device_type AND latest.key_id = hdsm.key_id
+      WHERE hd.device_id = p_device_id
+        AND hdsm.key_id = hkc.consumption_key_id
+        -- AND (hkc.emission_coeff IS NOT NULL AND hcp.param_name = hkc.emission_coeff)
+        AND hdsm.stat_time >= COALESCE(DATE_TRUNC('hour', latest.latest_stat_time), '1911-01-01')
+      GROUP BY 
+        truncated_stat_time, hdsm.device_id, stat_type, hdsm.device_type, hdsm.key_id, kd."key", hcp.float_value, hkc.emission_coeff
+      ORDER BY 
+        truncated_stat_time, hdsm.device_id, stat_type, hdsm.device_type, hdsm.key_id
+      ;
+
+  BEGIN
+  
+    SELECT CLOCK_TIMESTAMP() INTO v_start_time;
+    SELECT 0 INTO v_duration;
+
+    RAISE NOTICE '準備新增非電儀表 % 的時統計資料...', p_device_id;
+
+    -- 打開 Cursor
+    OPEN v_cursor;
+
+    -- 先讀取第一筆
+    FETCH v_cursor INTO v_rec;
+    
+    -- 若第一筆沒有資料，直接結束
+    IF NOT FOUND THEN
+        RAISE NOTICE '表中無資料';
+        CLOSE v_cursor;
+        RETURN;
+    END IF;
+
+    LOOP
+
+        -- 預先讀取下一筆資料
+        FETCH v_cursor INTO v_nrec;
+
+        -- 如果 `FOUND` 為 FALSE，表示這是最後一筆
+        IF NOT FOUND THEN
+            is_last := TRUE;
+        END IF;
+
+        INSERT INTO hd_device_statistics_hourly(stat_time, device_id, stat_type, device_type, key_id, dbl_stats, dbl_avg, dbl_min, dbl_max, kgco2e)
+        VALUES (v_rec.truncated_stat_time, v_rec.device_id, v_rec.stat_type, v_rec.device_type, v_rec.key_id, 
+                v_rec.dbl_stats, v_rec.dbl_avg, v_rec.dbl_min, v_rec.dbl_max, v_rec.kgco2e)
+        ON CONFLICT(stat_time, device_id, key_id, device_type) DO UPDATE SET
+              dbl_stats = EXCLUDED.dbl_stats,
+              dbl_avg = EXCLUDED.dbl_avg,
+              dbl_min = EXCLUDED.dbl_min,
+              dbl_max = EXCLUDED.dbl_max,
+              kgco2e = EXCLUDED.kgco2e              
+        ;
+
+        v_ins_rows := v_ins_rows + 1;
+
+        -- 只要是A. 目前 v_cursor 的最後一筆
+        -- 或者是B. 下一筆資料(v_nrec)的 device_id 或 key_id 有變，表示當前資料是同一個key_id的最後一筆
+        -- 不管是A,B，都要記錄到 hd_device_stat_hour_latest 當中
+        IF is_last /*OR (v_rec.device_id <> v_nrec.device_id OR v_rec.key_id <> v_nrec.key_id)*/ THEN
+            RAISE NOTICE '  id: %, time: % -> 最後一筆!', v_rec.device_id, v_rec.truncated_stat_time;
+
+            INSERT INTO public.hd_device_stat_hour_latest(device_id, device_type, key_id, latest_stat_time, latest_epoch)
+            VALUES (
+              v_rec.device_id, 
+              v_rec.device_type, 
+              v_rec.key_id, --v_rec.new_key_id,
+              v_rec.truncated_stat_time + INTERVAL '59 minutes 59 seconds',
+              EXTRACT(EPOCH FROM v_rec.truncated_stat_time + INTERVAL '59 minutes 59 seconds')*1000
+            )
+            ON CONFLICT(device_id, device_type, key_id) DO UPDATE SET
+              latest_stat_time = EXCLUDED.latest_stat_time,
+              latest_epoch = EXCLUDED.latest_epoch
+            ;
+        END IF;
+
+        -- 如果是最後一筆，結束迴圈
+        IF is_last THEN
+            EXIT;
+        END IF;
+
+        -- 將下一筆資料轉為當前記錄
+        v_rec := v_nrec;
+
+    END LOOP;    
+
+    SELECT EXTRACT(EPOCH FROM (CLOCK_TIMESTAMP()-v_start_time)) INTO v_duration;
+    RAISE NOTICE '  新增 % 的時統計資料，共新增 % 筆，計時 % 秒。', p_device_id, v_ins_rows, v_duration;
+
+    EXCEPTION
+          WHEN OTHERS THEN
+            -- 捕捉異常並記錄錯誤，不中斷主迴圈
+            RAISE EXCEPTION ' 處理設備 % 時發生錯誤，資料時間: %，原因: %。', p_device_id, v_rec.truncated_stat_time, SQLERRM;
+
+  END;
+$procedure$
+;
